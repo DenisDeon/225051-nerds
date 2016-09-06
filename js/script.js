59a8e707cc52484e32cc735eb8540bc666f631dc
DenisDeon <deonartist@gmail.com>
2016-09-06 16:56:00 +0300

    финал

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var link = document.querySelector(".discover-3");
+var popup = document.querySelector(".modal-content");
+var close = popup.querySelector(".modal-content-close");
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=name]");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal-content-show");
+  login.focus();
+});
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-content-show");
+});
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+});
